62741faadcf8de31697183fb43d1d9eb949c0df7
Shane Fournier <93808768+win32ss@users.noreply.github.com>
2022-11-21 15:54:36 -0500

    Add files via upload
    Removed unused code.

--- app.js
+++ app.js
@@ -149,9 +149,7 @@ function updateBookshelf() {
     // add book cards to the books grid
     for (book of readBook) {
         // current book's index in myLibrary
-		// pointer to current book for button-linked listener functions
         const index = i;
-		var book_ref = {book_obj: readBook};
 		let btnRead = [];
 		let btntoRead = [];
 		let btnReading = [];
@@ -226,8 +224,7 @@ function updateBookshelf() {
     // add book cards to the books grid
     for (book of readingBook) {
         // current book's index in myLibrary
-		// pointer to current book for button-linked listener functions
-		var book_ref = {book_obj: readingBook};
+
         const index = i;
 		let btnRead = [];
 		let btntoRead = [];
@@ -302,8 +299,6 @@ function updateBookshelf() {
     // add book cards to the books grid
     for (book of toreadBook) {
         // current book's index in myLibrary
-		// pointer to current book for button-linked listener functions
-		var book_ref = {book_obj: toreadBook};
         const index = i;
 		let btnRead = [];
 		let btntoRead = [];
@@ -378,8 +373,7 @@ function updateBookshelf() {
     // add book cards to the books grid
     for (book of recBook) {
         // current book's index in myLibrary
-		// pointer to current book for button-linked listener functions
-		var book_ref = {book_obj: recBook};
+
         const index = i;
 		let btnRead = [];
 		let btntoRead = [];
@@ -454,8 +448,7 @@ function updateBookshelf() {
     // add book cards to the books grid
     for (book of favBook) {
         // current book's index in myLibrary
-		// pointer to current book for button-linked listener functions
-		var book_ref = {book_obj: favBook};
+
         const index = i;
 		let btnRead = [];
 		let btntoRead = [];
@@ -535,57 +528,6 @@ function updateBookshelf() {
     updateStats();
 }
 
-function addBookCards(shelfNum,...arrayBook)
-{
-    //Clear shelf
-    booksGrid[shelfNum].replaceChildren();
-    console.log(arrayBook.length)
-    // add book cards to the books grid
-    for (const book of arrayBook) {
-        // current book's index in myLibrary
-        const index = document.querySelectorAll('.book-card').length;
-        //Create a book card css
-        bookCard = document.createElement('article');
-        bookCard.classList.add('book-card');
-        //Create a book title css
-        const bookTitle = document.createElement('p');
-        bookTitle.classList.add('book-title');
-        bookTitle.textContent = book.title;
-        //Create a book author css
-        const bookAuthor = document.createElement('p');
-        bookAuthor.classList.add('book-author');
-        bookAuthor.textContent = book.author;
-        //create book page css
-        const bookPages = document.createElement('p');
-        bookPages.classList.add('book-pages');
-        bookPages.textContent = `${book.pages} pages`;
-        //create 
-        const bookButtons = document.createElement('div');
-        bookButtons.classList.add('book-buttons');
-        bookButtons.dataset.libraryIndex = index;
-
-        const btnRead = document.createElement('button');
-        btnRead.classList.add('btn', 'btn-read');
-        btnRead.classList.add(book.read ? 'btn-green' : 'btn-red');
-        btnRead.textContent = (book.read ? "Read" : "Not read");
-        btnRead.addEventListener('click', function(){ toggleReadHandler(book_ref); });
-        bookButtons.appendChild(btnRead);
-
-        const btnDelete = document.createElement('button');
-        btnDelete.classList.add('btn', 'btn-delete');
-        btnDelete.textContent = "Delete";
-        btnDelete.addEventListener('click', deleteBook);
-        bookButtons.appendChild(btnDelete);
-
-        bookCard.appendChild(bookTitle);
-        bookCard.appendChild(bookAuthor);
-        bookCard.appendChild(bookPages);
-        bookCard.appendChild(bookButtons);
-        
-        booksGrid[shelfNum].appendChild(bookCard);
-           
-    }
-}
 
 const updateStats = () => {
     let totalCount = myLibrary.length;
@@ -617,50 +559,6 @@ const submitBook = () => {
     toggleForm();
 }
 
-// call toggleRead() method with the right Book object
-/*
-const toggleReadHandler = (e) => {
-    const index = e.target.parentNode.dataset.libraryIndex;
-    this.toggleRead();
-    if(this.read == true) {
-		this.toRead = false;
-		this.reading = false;
-	}
-	if(this.read == false && this.toRead == false && this.reading == false)
-	{
-		this.recommended = true;
-	}
-	console.log("toggleReadHandle called on:" + this.title);
-    updateBookshelf();
-}
-
-const toggletoReadHandler = (e) => {
-    const index = e.target.parentNode.dataset.libraryIndex;
-    myLibrary[index].toggletoRead();
-    if(myLibrary[index].toRead == true) {
-		myLibrary[index].read = false;
-		myLibrary[index].reading = false;
-	}
-	if(myLibrary[index].read == false && myLibrary[index].toRead == false && myLibrary[index].reading == false)
-	{
-		myLibrary[index].recommended = true;
-	}
-    updateBookshelf();
-}
-
-const toggleReadingHandler = (e) => {
-    const index = e.target.parentNode.dataset.libraryIndex;
-    myLibrary[index].toggleReading();
-    if(myLibrary[index].reading == true) {
-		myLibrary[index].toRead = false;
-		myLibrary[index].read = false;
-	}
-	if(myLibrary[index].read == false && myLibrary[index].toRead == false && myLibrary[index].reading == false)
-	{
-		myLibrary[index].recommended = true;
-	}
-    updateBookshelf();
-}*/
 
 const toggleForm = () => {
     bookForm.classList.toggle('active');
